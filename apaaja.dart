91b3ead1e535a7ea8f94a94f9002d8ae9b9451a5
alamrulam <alamruslam4831@gmail.com>
2024-03-06 15:57:50 +0700

    funtuinn

---
+++ apaaja.dart
@@ -0,0 +1,18 @@
+
+void main (){
+  print(hitungPersegi(5));
+
+
+}
+
+void cetakNama(){
+  print("Nama saya alam");
+}
+
+int hitungPersegi(int sisi){
+
+  int luas = sisi * sisi;
+  print(luas);
+  return luas;
+  
+}
